599f0c1876ff2f8753b0b5be633f605afdf3ff14
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-05-05 01:50:02 +0000

    Updating to latest FlutterFlow output.

---
+++ lib/components/card13_podcast_widget.dart
@@ -0,0 +1,88 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'package:flutter/material.dart';
+import 'card13_podcast_model.dart';
+export 'card13_podcast_model.dart';
+
+class Card13PodcastWidget extends StatefulWidget {
+  const Card13PodcastWidget({super.key});
+
+  @override
+  State<Card13PodcastWidget> createState() => _Card13PodcastWidgetState();
+}
+
+class _Card13PodcastWidgetState extends State<Card13PodcastWidget> {
+  late Card13PodcastModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => Card13PodcastModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 16.0),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: FlutterFlowTheme.of(context).secondaryBackground,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0.0, 12.0, 0.0, 0.0),
+                child: Text(
+                  'Podcast Title Goes Here',
+                  style: FlutterFlowTheme.of(context).titleLarge.override(
+                        fontFamily: 'Outfit',
+                        letterSpacing: 0.0,
+                      ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0.0, 12.0, 0.0, 12.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(8.0, 0.0, 0.0, 0.0),
+                      child: Text(
+                        'Mar 25 • 3 hr, 32 min',
+                        style:
+                            FlutterFlowTheme.of(context).labelMedium.override(
+                                  fontFamily: 'Readex Pro',
+                                  letterSpacing: 0.0,
+                                ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/satellities_page/satellities_page_widget.dart
+++ lib/pages/satellities_page/satellities_page_widget.dart
@@ -1,5 +1,8 @@
+import '/components/card13_podcast_widget.dart';
+import '/flutter_flow/flutter_flow_drop_down.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
 import 'package:flutter/material.dart';
 import 'satellities_page_model.dart';
 export 'satellities_page_model.dart';
@@ -38,11 +41,128 @@ class _SatellitiesPageWidgetState extends State<SatellitiesPageWidget> {
       child: Scaffold(
         key: scaffoldKey,
         backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: const SafeArea(
+        body: SafeArea(
           top: true,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [],
+          child: Padding(
+            padding: const EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding:
+                            const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 16.0),
+                        child: FlutterFlowDropDown<String>(
+                          controller: _model.dropDownValueController ??=
+                              FormFieldController<String>(null),
+                          options: const [
+                            'Satellite 1',
+                            'Satellite 2',
+                            'Satellite 3',
+                            'Satellite 4',
+                            'Satellite 5',
+                            'Satellite 6',
+                            'Satellite 7',
+                            'Satellite 8',
+                            'Satellite 9',
+                            'Satellite 10',
+                            'Satellite 11',
+                            'Satellite 12',
+                            'Satellite 13',
+                            'Satellite 14',
+                            'Satellite 15',
+                            'Satellite 16',
+                            'Satellite 17',
+                            'Satellite 18',
+                            'Satellite 19',
+                            'Satellite 20',
+                            'Satellite 21',
+                            'Satellite 22',
+                            'Satellite 23',
+                            'Satellite 24',
+                            'Satellite 25',
+                            'Satellite 26',
+                            'Satellite 27',
+                            'Satellite 28',
+                            'Satellite 29',
+                            'Satellite30',
+                            '31',
+                            '32'
+                          ],
+                          onChanged: (val) =>
+                              setState(() => _model.dropDownValue = val),
+                          width: 300.0,
+                          height: 56.0,
+                          textStyle:
+                              FlutterFlowTheme.of(context).bodyMedium.override(
+                                    fontFamily: 'Readex Pro',
+                                    letterSpacing: 0.0,
+                                  ),
+                          hintText: 'Please select...',
+                          icon: Icon(
+                            Icons.keyboard_arrow_down_rounded,
+                            color: FlutterFlowTheme.of(context).secondaryText,
+                            size: 24.0,
+                          ),
+                          fillColor:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          elevation: 2.0,
+                          borderColor: FlutterFlowTheme.of(context).alternate,
+                          borderWidth: 2.0,
+                          borderRadius: 8.0,
+                          margin: const EdgeInsetsDirectional.fromSTEB(
+                              16.0, 4.0, 16.0, 4.0),
+                          hidesUnderline: true,
+                          isOverButton: true,
+                          isSearchable: false,
+                          isMultiSelect: false,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SingleChildScrollView(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      wrapWithModel(
+                        model: _model.card13PodcastModel1,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                      wrapWithModel(
+                        model: _model.card13PodcastModel2,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                      wrapWithModel(
+                        model: _model.card13PodcastModel3,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                      wrapWithModel(
+                        model: _model.card13PodcastModel4,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                      wrapWithModel(
+                        model: _model.card13PodcastModel5,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                      wrapWithModel(
+                        model: _model.card13PodcastModel6,
+                        updateCallback: () => setState(() {}),
+                        child: const Card13PodcastWidget(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
